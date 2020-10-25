26e301fb1d5b7bba50fa8bd370a538fefa884037
iknowright <changchaishi@gmail.com>
2020-10-25 15:56:30 +0800

    feat: modulize project files by creating subfolders

---
+++ dataset_helper/data_analysis.py
@@ -0,0 +1,26 @@
+def load_data():
+    with open("kaggle.csv", "r") as fo:
+        lines = fo.readlines()
+        data = [line.strip().split(",") for line in lines]
+        return data
+
+def find_unique(data):
+    items = set()
+    for itemset in data:
+        items = set.union(items, itemset)
+    items = list(items)
+    items.sort()
+    return items
+
+def market_basket_optimization():
+    data = load_data()
+    data_len = len(data)
+    print(data_len)
+    items = find_unique(data)
+    print(len(items))
+    average_items_in_set = round(sum([len(itemset) for itemset in data]) / data_len, 2)
+    print(average_items_in_set)
+
+
+if __name__ == "__main__":
+    market_basket_optimization()
